df3c8a3f07c1496c35a05a77b12f43b45ee8da55
Jonbo <jon.bosque.hernando@gmail.com>
2018-05-21 19:13:51 +0200

    write chain to file

--- scripts/spy_spx_datacollection.py
+++ scripts/spy_spx_datacollection.py
@@ -1,18 +1,20 @@
 import time
+import datetime
+import os
 
 from ibapi import contract
 
 import options_collector as col
 
-# info from: https://pennies.interactivebrokers.com/cstools/contract_info/
+dateformat = "%Y%m%d%H%M%S"
 
+# info from: https://pennies.interactivebrokers.com/cstools/contract_info/
 spy = contract.Contract()
 spy.conId = 756733
 spy.symbol = "SPY"
 spy.exchange = "SMART"
 spy.secType = "STK"
 spy.currency = "USD"
-
 
 spx = contract.Contract()
 spx.conId = 416904
@@ -21,22 +23,32 @@ spx.exchange = "SMART"
 spx.secType = "IND"
 spx.currency = "USD"
 
-
 collector = col.OptionsCollector()
 collector.request_generic_info(spy)
 spy_expirations = collector.wrapperObj.available_expirations
 collector.request_generic_info(spx)
 spx_expirations = collector.wrapperObj.available_expirations
-# SPY LOOP
+
+
+if not os.path.isdir('SPY'):
+    os.mkdir('SPY')
+if not os.path.isdir('SPX'):
+    os.mkdir('SPX')
+
 while True:
+    # SPY LOOP
     for expiration in spy_expirations:
         print('Collecting SPY ' + expiration)
         collector.subscription(spy, int(expiration))
         time.sleep(10)
+        collector.retrieve_option_chain().to_csv(
+            'SPY/' + 'SPY_' + expiration + '_' + datetime.datetime.now().strftime(dateformat) + '.csv')
         collector.disconnect_subscription()
-
+    # SPX LOOP
     for expiration in spx_expirations:
         print('Collecting SPX ' + expiration)
         collector.subscription(spx, int(expiration))
         time.sleep(10)
+        collector.retrieve_option_chain().to_csv(
+            'SPX/' + 'SPX_' + expiration + '_' + datetime.datetime.now().strftime(dateformat) + '.csv')
         collector.disconnect_subscription()
